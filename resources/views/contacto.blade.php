3a08d0dab2f11a42c01f7f2489be05ccb155dc36
Sebastian Mata <sebasmata41@gmail.com>
2024-01-03 19:17:11 -0400

    second comit

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -2,14 +2,21 @@
 @section('title','Contacto')
 
 @section('body')
-<section id='hero'style="background-image:url('/assets/images/black.png')">
+<section id='hero'>
     <h1 style="margin-top: 50px; width: 1000px; align-self: center">
         En este apartado puede encontrar links a mis redes sociales
     </h1>
 
     <div>
-        <ul>
-            <li><a href="#">Spotify</a></li>
+        <ul style="margin-top: 30px">
+            <li><a  href="https://www.linkedin.com/in/sebastián-mata-195704274/">
+                    <img src="{{asset('assets/images/linkedin.png')}}" width="30px">
+                </a>
+                <a style="padding: 20px"  href="https://www.instagram.com/s14vc/">
+                    <img src="{{asset('assets/images/instagram.png')}}" width="30px">
+                </a>
+            </li>
+
         </ul>
     </div>
 
